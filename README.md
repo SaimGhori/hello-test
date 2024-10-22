24349a7b64dd82ee291f18d8971ba8fce50993d9
SaimGhori <23204009@apsit.edu.in>
2024-10-22 14:21:20 +0530

    Commiting from the branch file

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # hello-test
 To Perform practical on branches in Git
+Test Text thats added into a different branch called feature
--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
 listt = ["Hello", "World"]
 for x in listt:
   print(x, end=" ")
+Test Text thats added into a different branch called feature
